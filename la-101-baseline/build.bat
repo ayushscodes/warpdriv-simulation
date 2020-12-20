5deb77392fc5d67a2aa9197e877751635bcb75ba
Ayush Sharma <ayushs@Ayushs-MacBook-Air.local>
2020-12-20 13:49:03 -0800

    LA - cars, bus, trains baseline simulation

--- la-101-baseline/build.bat
+++ la-101-baseline/build.bat
@@ -1,3 +1,2 @@
 #!/bin/bash
-python3 "$SUMO_HOME/tools/randomTrips.py"  -n osmSanitized.net.xml --fringe-factor 10 -p 0.258375 -o osmSanitized.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 10000 --trip-attributes "departLane=\"random\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --validate -l -L
-3
+python3 $SUMO_HOME/tools/randomTrips.py  -n osmSanitized.net.xml --fringe-factor 10 -p 0.258375 -o osmSanitized.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 3000 --trip-attributes "departLane=\"random\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --validate -l -L --speed-exponent=1 --binomial=4
--- la-101-baseline/commands.sh
+++ la-101-baseline/commands.sh
@@ -1,2 +1,8 @@
 # run sumo cli
-$SUMO_HOME/bin/sumo -c osm.sumocfg -b 0 -e 600 --threads 4 -t 
+$SUMO_HOME/bin/sumo -c osm.sumocfg -b 0 -e 3600 --threads 4 -t 
+
+# Random trips (length, lane, speed weighting | binomial arrival w rate=4)
+python3 $SUMO_HOME/tools/randomTrips.py  -n osmSanitized.net.xml --fringe-factor 10 -p 0.25 -o osmSanitized.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 3000 --trip-attributes "departLane=\"random\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --validate -l -L
+
+# Gaussian random trips | 3600 s total time | mean = 1800 | std = 600 | peak_vehicle_rate = 10
+python3 $SUMO_HOME/tools/randomTripsModified.py  -n osmSanitized.net.xml --fringe-factor 10 -o osmSanitizedGaussian.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 3000 --trip-attributes "departLane=\"random\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --validate -l -L --mu 1800 --sigma 600 --peak-vehicle-rate 10 
